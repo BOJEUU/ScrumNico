b1e0b77e366c48c06631cbe44c5af445291af46a
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-15 15:34:18 +0100

    vico pull nico flav

--- index.php
+++ index.php
@@ -68,12 +68,8 @@ if (isset($_GET["page"]) && $_GET["page"] == "contact") {
         echo $xml->page[$show]->content;
     }
     ?>
-<<<<<<< HEAD
-  
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-=======
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
->>>>>>> 9aade046bfd0777aaf26cdf5b29aa9172372fad7
 
 </body>
 
