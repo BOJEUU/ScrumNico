bacf0d8b8d67739f1309ce07d1c5761d98c0a4d4
Flavie-LHP5 <flaviefromentlhp5@gmail.com>
2021-01-15 15:07:34 +0100

    flav

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_GET["page"]) && $_GET["page"] == "contact") {
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand  text-light" href="#">Maçonnerie Ocordo</a>
+            <a class="navbar-brand  text-white" href="#">Maçonnerie Ocordo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -59,6 +59,7 @@ if (isset($_GET["page"]) && $_GET["page"] == "contact") {
          echo $xml->page[$show]->content;
     }
     ?>
+  
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 </body>
