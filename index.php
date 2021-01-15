4c6d5109f6521f047403c5d16a8f68eec8a0d434
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-15 13:47:24 +0100

    titre contact

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_GET["page"]) && $_GET["page"] == "contact") {
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand  text-light" href="#">Maçonnerie Ocordo</a>
+            <a class="navbar-brand  text-light">Maçonnerie Ocordo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
