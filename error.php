d5b31422acf490f60c1366eee6170f46a89599b7
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-18 11:12:50 +0100

    maj page d'erreur

---
+++ error.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>ORCODO TESTE NICO</title>
+</head>
+
+<body>
+<div class="d-flex p-5 justify-content-center">
+<img src="assets/img/a22.jpg">
+</div>
+    <footer>
+        <div class="container-fluid text-center bg-dark shadow text-white  ">
+            <div class="row">
+                <div class="col-sm mt-3 mb-5">
+                    Réseaux sociaux
+                    <div>
+                        <i class="fab fa-facebook "></i>
+                        <i class="fab fa-linkedin"></i>
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                </div>
+                <div class="col-sm mt-3">
+                <i class="far fa-copyright"> Copyright : Nicolas, Houlé, Victor, Flavie: 2021</i>
+                <div>
+                    Plus les 20 autres personnes dans la tête de Victor. <i class="far fa-smile"></i>
+                </div>
+                </div>
+                <div class="col-sm mt-3">
+                    Contact
+                    <div>
+                        <i class="fas fa-phone-square-alt"> 02.51.84.18.24</i>
+                        <div>
+                            <i class="fas fa-envelope-square"> contact@ocordo-travaux.fr</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
